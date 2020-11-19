90b4504a5b6e73c352465855679b18e719f681ef
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-11-19 08:32:04 +0530

    Add files via upload

---
+++ Interview Kit/Valley_Counting.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    count = 0
+    result = 0
+    for c in path :
+      
+        if c == 'U':
+            count = count + 1
+        else:
+            count = count - 1
+        if count == 0 and c == 'U':
+            result = result +1  
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
